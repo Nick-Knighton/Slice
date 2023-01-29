543b912b4a9e0c3eb67bdcb828c019b9edbc300d
zacz <zacz@tamu.edu>
2023-01-29 00:04:15 -0600

    background changes

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -1,10 +1,10 @@
 function Page() {
     return (
-        <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <div class="text-2xl font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 bg-gradient-to-r from-cyan-500 to-blue-500">
                 <li class="mr-2 flex flex-row justify-center">
-                    <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
+                    <a href="#" class="inline-block  p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
                 </li>
-            <ul class="flex flex-row justify-center" >
+            <ul class="flex flex-row justify-center bg-gradient-to-r from-cyan-500 to-blue-500" >
                 <li class="mr-2">
                     <a href="#" class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Payment</a>
                 </li>
@@ -15,6 +15,10 @@ function Page() {
                     <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">History</a>
                 </li>
             </ul>
+        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+            </div>
+
+{/* </div> */}
 </div>
     )
 } 
