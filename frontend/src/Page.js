f1b18b4837e20802cca342068139b72b3f3a9069
Nick Knighton <nickk@tamu.edu>
2023-01-29 01:20:01 -0600

    new buttons

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -56,22 +56,20 @@ function Page() {
                     </div>
                 </div>
 
-                <div class="flex justify-center bg-white">
-                    <div>
-                        <div class="form-check">
-                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2" type="checkbox" value="" id="flexCheckDisabled" disabled/>
-                        <label class="form-check-label inline-block text-gray-800 opacity-50" for="flexCheckDisabled">
-                            Disabled checkbox
-                        </label>
-                        </div>
-                        <div class="form-check">
-                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2" type="checkbox" value="" id="flexCheckCheckedDisabled" checked disabled/>
-                        <label class="form-check-label inline-block text-gray-800 opacity-50" for="flexCheckCheckedDisabled">
-                            Disabled checked checkbox
-                        </label>
-                        </div>
+                <div class="flex justify-center flex-col bg-white mt-5 h-32">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="inlineCheckbox1" value="option1"/>
+                        <label class="form-check-label inline-block text-gray-800" for="inlineCheckbox1">Nick</label>
                     </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="inlineCheckbox2" value="option2"/>
+                        <label class="form-check-label inline-block text-gray-800" for="inlineCheckbox2">Zac</label>
                     </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2" type="checkbox" id="inlineCheckbox3" value="option3" disabled/>
+                        <label class="form-check-label inline-block text-gray-800 opacity-50" for="inlineCheckbox3">Will (disabled)</label>
+                    </div>
+                </div>
 
             </div>
 
