b928ce9e27b90ed372f1ab5c604c61402449fe63
Nick Knighton <nickk@tamu.edu>
2023-01-29 01:16:12 -0600

    check boxes

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -56,6 +56,23 @@ function Page() {
                     </div>
                 </div>
 
+                <div class="flex justify-center bg-white">
+                    <div>
+                        <div class="form-check">
+                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2" type="checkbox" value="" id="flexCheckDisabled" disabled/>
+                        <label class="form-check-label inline-block text-gray-800 opacity-50" for="flexCheckDisabled">
+                            Disabled checkbox
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2" type="checkbox" value="" id="flexCheckCheckedDisabled" checked disabled/>
+                        <label class="form-check-label inline-block text-gray-800 opacity-50" for="flexCheckCheckedDisabled">
+                            Disabled checked checkbox
+                        </label>
+                        </div>
+                    </div>
+                    </div>
+
             </div>
 
         </div>
