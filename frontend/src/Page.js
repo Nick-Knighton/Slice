b81bc0b22451ae1603155504011e88d1643fa25b
Nick Knighton <nickk@tamu.edu>
2023-01-28 23:47:19 -0600

    added page

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -1,4 +1,21 @@
 function Page() {
-    return <h1>1234</h1>
+    return (
+        <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                <li class="mr-2 flex flex-row justify-center">
+                    <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
+                </li>
+            <ul class="flex flex-row justify-center" >
+                <li class="mr-2">
+                    <a href="#" class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Payment</a>
+                </li>
+                <li class="mr-2">
+                    <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Money</a>
+                </li>
+                <li class="mr-2">
+                    <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">History</a>
+                </li>
+            </ul>
+</div>
+    )
 } 
 export default Page;
