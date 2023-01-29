50c5953a5d6883031dcabbbd6c30fb99ae948831
Nick Knighton <nickk@tamu.edu>
2023-01-29 02:07:59 -0600

    Changes

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 
 
 function Page() {
+    const [user, setUser] = useState('');
+    const [amountPayed, setamountPayed] = useState('');
+    const [debtors, setDebptors] = useState('');
     
-    const [value, setValue] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    const handleChange = (event) => {
-      setValue(event.target.value);
+        console.log("User:", user);
+        console.log("Amount:", amountPayed);
+        console.log("Debtors:", debtors);
     };
+
     
     
     return (
@@ -30,27 +36,24 @@ function Page() {
 
             </div>
 
-            <div id='form elements'className="h-full flex flex-col justify-center items-center">
-                <div>
-                    <div class="relative mt-1 rounded-md shadow-sm">
-                        <select id="currency" name="currency" class="h-full rounded-md border-transparent bg-white py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ">
-                            <option>Nick</option>
-                            <option>Zac</option>
-                            <option>Will</option>
-                        </select>
-                    </div>
+            <form id='form elements'className="h-full flex flex-col justify-center items-center" onSubmit={handleSubmit}>
+                <div class="relative mt-1 rounded-md shadow-sm">
+                    <select id="currency" name="currency" class="h-full rounded-md border-transparent bg-white py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm " value={user} onChange={(event) => {setUser(event.target.value)}}>
+                        <option>Nick</option>
+                        <option>Zac</option>
+                        <option>Will</option>
+                    </select>
                 </div>
                 
                 <div>
                     <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
                     <div class="relative mt-1 rounded-md shadow-sm">
                         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span class="text-gray-500 sm:text-sm">$</span>
+                            <span class="text-gray-500 sm:text-sm">$</span>
                         </div>
-                        <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00"/>
-                        <div class="absolute inset-y-0 right-0 flex items-center">
-                        <label for="currency" class="sr-only">Currency</label>
-                        </div>
+                        <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00" value={amountPayed} onChange={(event) => {
+                            setamountPayed(event.target.value);
+        }}/>
                     </div>
                 </div>
 
@@ -85,11 +88,11 @@ function Page() {
                     </div>
                 </div>
                 
-                <button class="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full">
+                <button type="submit" class="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full mt-5">
                     1234
                 </button>
 
-            </div>
+            </form>
 
 
         </div>
