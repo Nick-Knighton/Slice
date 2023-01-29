a395081eda2793fdba250774a67c4040694a621b
zacz <zacz@tamu.edu>
2023-01-29 02:06:18 -0600

    style changes to home page

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -33,7 +33,7 @@ function Page() {
             <div id='form elements'className="h-full flex flex-col justify-center items-center">
                 <div>
                     <div class="relative mt-1 rounded-md shadow-sm">
-                        <select id="currency" name="currency" class="h-full rounded-md border-transparent bg-white py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ">
+                        <select id="currency" name="currency" class="h-full rounded-md border-transparent bg-white py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-3xl ">
                             <option>Nick</option>
                             <option>Zac</option>
                             <option>Will</option>
@@ -42,19 +42,19 @@ function Page() {
                 </div>
                 
                 <div>
-                    <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+                    <label for="price" class="block text-sm font-medium text-gray-700 sm:text-3xl">Price</label>
                     <div class="relative mt-1 rounded-md shadow-sm">
                         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span class="text-gray-500 sm:text-sm">$</span>
+                        <span class="text-gray-500 sm:text-3xl">$</span>
                         </div>
-                        <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00"/>
+                        <input type="text" name="price" id="price" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-3xl" placeholder="0.00"/>
                         <div class="absolute inset-y-0 right-0 flex items-center">
                         <label for="currency" class="sr-only">Currency</label>
                         </div>
                     </div>
                 </div>
 
-                <div class="flex justify-center flex-col bg-white mt-5 h-32 overflow-auto">
+                <div class="flex justify-center flex-col bg-white mt-5 h-32 overflow-auto sm:text-3xl">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="inlineCheckbox1" value="option1"/>
                         <label class="form-check-label inline-block text-gray-800" for="inlineCheckbox1">Nick</label>
@@ -85,7 +85,7 @@ function Page() {
                     </div>
                 </div>
                 
-                <button class="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full">
+                <button class="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 mt-3 rounded-full sm:text-3xl ">
                     1234
                 </button>
 
